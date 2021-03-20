78aae49b4c5e40fdb62c0270f43e40f3dd72c0e3
Luffrbot <80769361+Luffrbot@users.noreply.github.com>
2021-03-19 23:05:09 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,220 @@
+const help = (prefix) => {
+	return `
+
+           〘 𝙈𝙄𝘿𝙊𝙍𝙄𝙔𝘼 𝘽𝙊𝙏🧸 〙
+
+║〘 INFORMAÇÕES 〙
+║
+║➥ 𝙈𝙄𝘿𝙊𝙍𝙄𝙔𝘼 𝘽𝙊𝙏 🧸
+║➥𝘿𝙊𝙉𝙊:Kratosツ *wa.me/+558694292223*
+║➥𝘿𝙤𝙖𝙘̧𝙤̃𝙚𝙨 0.00$🙂
+║
+║➣ 𝙈𝙀𝙉𝙐 𝙂𝙀𝙍𝘼𝙇📚
+║
+║
+║➣ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙁𝙄𝙂𝙐𝙍𝙄𝙉𝙃𝘼𝙎🛹
+║ ➥ *${prefix}sticker*
+║ ➥ *${prefix}toimg*
+║ ➥ *${prefix}darkjokes (memes aleatórios)*
+║ ➥ *${prefix}memeindo*
+║ ➥ *${prefix}tts*
+║ ➥ *${prefix}lolih [on]*
+║ ➥ *${prefix}nsfwloli [off]*
+║ ➥ *${prefix}url2img*
+║ ➥ *${prefix}leens [na legenda]*
+║ ➥ *${prefix}wait [na legenda]*
+║ ➥ *${prefix}setprefix*
+║
+║➣ 𝙊𝙐𝙏𝙍𝙊𝙎🌿
+║
+║ ➥ *${prefix}linkgp*
+║ ➥ *${prefix}simih [1/0]*
+║ ➥ *${prefix}marcar*
+║ ➥ *${prefix}add [@]*
+║ ➥ *${prefix}banir [@]*
+║ ➥ *${prefix}promover [@]*
+║ ➥ *${prefix}rebaixar*
+║ ➥ *${prefix}admins*
+║ ➥ *${prefix}marcar2*
+║ ➥ *${prefix}bc [texto]* (ele faz uma ™)
+║ ➥ *${prefix}marcar3*
+║ ➥ *${prefix}bloqueados*
+║ ➥ *${prefix}bloquear [@]*
+║ ➥ *${prefix}desbloquear [@]*
+║ ➥ *${prefix}limpar*
+║ ➥ *${prefix}bc [ *texto* ]*
+║ ➥ *${prefix}bemvindo [1/0]*
+║ ➥ *${prefix}clonar [@]*
+║ ➥ *${prefix}help1*
+║ ➥ *${prefix}dono*
+║ ➥ *${prefix}owner*
+║ ➥ *${prefix}tts [texto]*
+║ ➥ *${prefix}setnome*
+║ ➥ *${prefix}termux*
+║ ➥ *${prefix}setfoto*
+║ ➥ *${prefix}grupoinfo*
+║ ➥ *${prefix}ytmp4*
+║ ➥ *${prefix}bomdia*
+║ ➥ *${prefix}boanoite*
+║ ➥ *${prefix}marcar*
+║ ➥ *${prefix}marcar2*
+║ ➥ *${prefix}marcar3*
+║
+║➣ 𝙄𝙈𝘼𝙂𝙀𝙉𝙎🏝️
+║
+║ ➥ *${prefix}loli* [off]
+║ ➥ *${prefix}loli1*
+║ ➥ *${prefix}hentai*
+║ ➥ *${prefix}dono*
+║ ➥ *${prefix}porno*
+║ ➥ *${prefix}boanoite*
+║ ➥ *${prefix}bomdia*
+║ ➥ *${prefix}boatarde*
+║ ➥ *${prefix}mia [aleatórias]*
+║ ➥ *${prefix}rize [aleatórias]*
+║ ➥ *${prefix}minato [aleatórias]*
+║ ➥ *${prefix}boruto [aleatórias]*
+║ ➥ *${prefix}hinata [aleatórias]*
+║ ➥ *${prefix}sasuke [aleatórias]*
+║ ➥ *${prefix}sakura [aleatórias]*
+║ ➥ *${prefix}naruto [aleatórias]*
+║ ➥ *${prefix}meme*   
+║ ➥ *${prefix}lofi*
+║ ➥ *${prefix}malkova*
+║ ➥ *${prefix}canal*
+║ ➥ *${prefix}nsfwloli1*
+║ ➥ *${prefix}reislin*
+║
+║➣ 𝙄𝙉𝙏𝙀𝙇𝙄𝙂𝙀𝙉𝘾𝙄𝘼🤖
+║
+║ ➥ *${prefix}simih 1 (para ativar)*
+║ ➥ *${prefix}simih 0 (para desativar)*
+║ *${prefix}simi (sua mensagem)*
+║
+║➣ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝙀𝙈 𝙏𝙀𝙎𝙏𝙀📙
+║
+║ ➥ *${prefix}gado*
+║ ➥ *${prefix}dbz*
+║ ➥ *${prefix}gtts loli*
+║ ➥ *${prefix}hidegp*
+║
+║➣ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈🧃
+║
+║ ➥ *${prefix}dado*
+║ ➥ *${prefix}cekvip*
+║ ➥ *${prefix}premiumlist*
+║ ➥ *${prefix}delete*
+║ ➥ *${prefix}modapk*
+║ ➥ *${prefix}indo10*
+║ ➥ *${prefix}daftarvip [para virar Premium]*
+║ ➥ *${prefix}qrcode*
+║ ➥ *${prefix}chentai*
+║ ➥ *${prefix}gcpf*
+║ ➥ *${prefix}gbin*
+║ ➥ *${prefix}pack*
+║ ➥ *${prefix}destrava*
+║ ➥ *${prefix}gpessoa*
+║
+║➣ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙂𝙍𝙐𝙋𝙊𝙎!🍂
+║
+║ ➥ *${prefix}banir*
+║ ➥ *${prefix}leveling [on/off]*
+║ ➥ *${prefix}level*
+║ ➥ *${prefix}add*
+║ ➥ *${prefix}promover*
+║ ➥ *${prefix}setfoto [na legenda]*
+║ ➥ *${prefix}setname [texto]*
+║ ➥ *${prefix}rebaixar*
+║ ➥ *${prefix}admins*
+║ ➥ *${prefix}marcar*
+║ ➥ *${prefix}marcar2*
+║ ➥ *${prefix}marcar3*
+║ ➥ *${prefix}bemvindo [1/0]*
+║ ➥ *${prefix}grupoinfo*
+║ ➥ *${prefix}bomdia*
+║ ➥ *${prefix}boatarde*
+║ ➥ *${prefix}boanoite*
+║ ➥ *${prefix}setdesc*
+║ ➥ *${prefix}bug [sua mensagem]*
+║
+║➣ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝙎𝙄𝙈𝙋𝙇𝙀𝙎 𝘿𝙊 𝘽𝙊𝙏🍇
+║
+║ ➥ *${prefix}bug [sua mensagem]*
+║ ➥ *${prefix}clonar [@]*
+║ ➥ *${prefix}dono*
+║ ➥ *${prefix}ping [ver velocidade do bot]*
+║ ➥ *${prefix}termux*
+║ ➥ *${prefix}gay [@]*
+║ ➥ *${prefix}wame*
+║ ➥ *${prefix}map (nome)*
+║ ➥ *${prefix}setppbot (marque uma img)*
+║ ➥ *${prefix}pinterest (nome)*
+║ ➥ *${prefix}desligar (so para o dono)*
+║ ➥ *${prefix}timer*
+║
+║➣ 𝙈𝘼𝙄𝙎 𝘼𝙇𝙂𝙐𝙉𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎🔍
+║
+║ ➥ *${prefix}neko*
+║ ➥ *${prefix}ttp [texto]*
+║ ➥ *${prefix}testime*
+║ ➥ *${prefix}tomp3*
+║ ➥ *${prefix}modoanime [on/off]*
+║ ➥ *${prefix}modonsfw [on/off]*
+║ ➥ *${prefix}happymod [jogo/app]*
+║ ➥ *${prefix}rize*
+║ ➥ *${prefix}ytsearch*
+║ ➥ *${prefix}moddroid [jogo/app]*
+║ ➥ *${prefix}xvideos [titulo]**
+║ ➥ *${prefix}nomegp*
+║ ➥ *${prefix}darkjokes (memes aleatórios)*
+║ ➥ *${prefix}animecry*
+║ ➥ *${prefix}gay1*
+║ ➥ *${prefix}next*
+║ ➥ *${prefix}alerta*
+║ ➥ *${prefix}belle [img aleatórias]*
+║ ➥ *${prefix}pronomeneu [texto]*
+║ ➥ *${prefix}hobby*
+║
+║➣ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙑𝙊𝙕🎤
+║
+║ ➥ *${prefix}ola*
+║ ➥ *${prefix}bv*
+║ ➥ *${prefix}tchau*
+║ ➥ *${prefix}bem*
+║ ➥ *${prefix}a*
+║ ➥ *${prefix}fdp*
+║ ➥ *${prefix}onich*
+║ ➥ *${prefix}beat1*
+║ ➥ *${prefix}glub*
+║
+║➣ 𝙊𝙐𝙏𝙍𝙊𝙎 1/0🥋
+║
+║ ➥ *${prefix}antilink [1/0]*
+║ ➥ *${prefix}brainly [pergunta]*
+║ ➥ *${prefix}antiracismo [on/off]*
+║ ➥ *${prefix}setnomebot*
+║ ➥ *${prefix}meme*
+║
+║➣ 𝙄𝙉𝙏𝙀𝙍𝘼𝙏𝙄𝙑𝙊𝙎🎭
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║ ➥ *bah*
+║ ➥ *oii*
+║ ➥ *bv*
+║ ➥ *canta ai bot*
+║ ➥ *grita*
+║ ➥ *bot lixo*
+║
+║➣ 𝙈𝙖𝙞𝙨 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘̧𝙤̃𝙚𝙨
+║
+║ *SE quise coloca o bot no seu Grupo!! Chame o Dono do bot*
+║  
+║
+╚═〘 𝙈𝙄𝘿𝙊𝙍𝙄𝙔𝘼 𝘽𝙊𝙏🧸〙`
+}
+
+exports.help = help
+
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,ETC...*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/558694292223*
+║
+╚═〘  MIDORIYA BOT  〙
+`
+}
+exports.iklan = iklan
